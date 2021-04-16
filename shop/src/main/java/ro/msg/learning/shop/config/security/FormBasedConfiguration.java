b12b94e7c26edb57ca2b165bd63a89afa682bf21
Andrei Brehuescu <Andrei.Brehuescu@msg.group>
2021-04-16 10:33:27 +0300

    Finished security

--- shop/src/main/java/ro/msg/learning/shop/config/security/FormBasedConfiguration.java
+++ shop/src/main/java/ro/msg/learning/shop/config/security/FormBasedConfiguration.java
@@ -1,28 +1,29 @@
 package ro.msg.learning.shop.config.security;
 
 import lombok.AllArgsConstructor;
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import ro.msg.learning.shop.service.implementation.UserDetailsService;
 
-
 @Configuration
 @EnableWebSecurity
-@Profile("with-basic")
-public class BasicConfiguration extends WebSecurityConfigurerAdapter {
+@AllArgsConstructor
+@Profile("with-form")
+public class FormBasedConfiguration extends WebSecurityConfigurerAdapter {
 
     private UserDetailsService userDetailsService;
 
     @Override
-    public void configure(WebSecurity web) {
+    public void configure(WebSecurity web){
         web.ignoring().antMatchers("/h2-console/**");
     }
 
@@ -39,13 +40,18 @@ public class BasicConfiguration extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/h2-console").permitAll()
-                .antMatchers("/**").authenticated()
+                .anyRequest()
+                .authenticated()
                 .and()
-                .httpBasic()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .invalidateHttpSession(true)
+                .permitAll()
                 .and()
                 .csrf().disable();
         http.headers().frameOptions().disable();
-
     }
+
 }
